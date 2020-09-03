9569c482e88c90bae3f47de412a78a18f04048ee
Hitesh <hitgo17@gmail.com>
2020-09-04 03:21:55 +0530

    feat: adds inner planets to the mix 🌍
    Signed-off-by: Hitesh <hitgo17@gmail.com>

--- sketch.js
+++ sketch.js
@@ -27,8 +27,8 @@ const sketch = ({ context }) => {
   renderer.setClearColor('#000', 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
-  camera.position.set(12, 2, -14);
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 2000);
+  camera.position.set(21, 2, -100);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -39,6 +39,7 @@ const sketch = ({ context }) => {
 
   // Setup a geometry
   const geometry = new THREE.SphereGeometry(1, 32, 16);
+  const ringGeometry = new THREE.RingGeometry(0.5, 0.7, 64);
 
   const loader = new THREE.TextureLoader();
   const textureEarth = loader.load('earth.jpg');
@@ -46,6 +47,10 @@ const sketch = ({ context }) => {
   const sunTexture = loader.load('sun.jpg');
   const marsTexture = loader.load('mars.jpg');
   const jupiterTexture = loader.load('jupiter.jpg');
+  const mercuryTexture = loader.load('mercury.jpg');
+  const venusTexture = loader.load('venus.jpg');
+  const saturnTexture = loader.load('saturn.jpg');
+  const saturnRingTexture = loader.load('saturn_ring.png');
 
   const milkywayTexture = loader.load('milky_way.jpg');
   // milkywayTexture.wrapS = milkywayTexture.wrapT = THREE.RepeatWrapping;
@@ -73,6 +78,19 @@ const sketch = ({ context }) => {
     metalness: 0,
     map: sunTexture,
   });
+  const mercuryMaterial = new THREE.MeshStandardMaterial({
+    roughness: 1,
+    metalness: 0,
+    map: mercuryTexture,
+  });
+  const mercuryGroup = new THREE.Group();
+
+  const venusMaterial = new THREE.MeshStandardMaterial({
+    roughness: 1,
+    metalness: 0,
+    map: venusTexture,
+  });
+  const venusGroup = new THREE.Group();
 
   const marsMaterial = new THREE.MeshStandardMaterial({
     roughness: 1,
@@ -88,9 +106,26 @@ const sketch = ({ context }) => {
   });
   const jupiterGroup = new THREE.Group();
 
+  const saturnMaterial = new THREE.MeshStandardMaterial({
+    roughness: 1,
+    metalness: 0,
+    map: saturnTexture,
+  });
+
+  const saturnGroup = new THREE.Group();
+
+  saturnRingTexture.wrapS = saturnRingTexture.wrapT = THREE.RepeatWrapping;
+  saturnRingTexture.repeat.set(4, 3);
+  const saturnRingMaterial = new THREE.MeshStandardMaterial({
+    roughness: 1,
+    metalness: 0,
+    map: saturnRingTexture,
+  });
+  const saturnRingGroup = new THREE.Group();
+
   // Setup a mesh with geometry + material
   const earthMesh = new THREE.Mesh(geometry, earthMaterial);
-  earthMesh.position.set(10, 1, 0);
+  earthMesh.position.set(35, 1, 0);
   earthGroup.add(earthMesh);
   // scene.add(earthMesh);
 
@@ -101,23 +136,49 @@ const sketch = ({ context }) => {
   earthMesh.add(moonGroup);
 
   const marsMesh = new THREE.Mesh(geometry, marsMaterial);
-  marsMesh.position.set(15, 1.2, 0);
+  marsMesh.position.set(49, 1.2, 0);
   marsGroup.add(marsMesh);
 
+  const mercuryMesh = new THREE.Mesh(geometry, mercuryMaterial);
+  mercuryMesh.scale.setScalar(0.5);
+  mercuryMesh.position.set(14, 1.2, 0);
+  mercuryGroup.add(mercuryMesh);
+
+  const venusMesh = new THREE.Mesh(geometry, venusMaterial);
+  venusMesh.position.set(26, 1.2, 0);
+  venusGroup.add(venusMesh);
+
   const jupiterMesh = new THREE.Mesh(geometry, jupiterMaterial);
-  jupiterMesh.position.set(22, -1, 0);
-  jupiterMesh.scale.setScalar(1.5);
+  jupiterMesh.position.set(70, -1, 0);
+  jupiterMesh.scale.setScalar(3.5);
   jupiterGroup.add(jupiterMesh);
+
+  const saturnMesh = new THREE.Mesh(geometry, saturnMaterial);
+  saturnMesh.position.set(91, -1, 0);
+  saturnMesh.scale.setScalar(2.8);
+  saturnGroup.add(saturnMesh);
+
+  const saturnRingMesh = new THREE.Mesh(ringGeometry, saturnRingMaterial);
+  saturnRingMesh.material.side = THREE.DoubleSide;
+  saturnRingMesh.position.set(0, 0, 0);
+  saturnRingMesh.scale.setScalar(2.8);
+  saturnRingMesh.rotation.x = Math.PI / 3;
+  saturnRingGroup.add(saturnRingMesh);
+  saturnMesh.add(saturnRingGroup);
 
   const sunMesh = new THREE.Mesh(geometry, sunMaterial);
   sunMesh.position.set(0, 0, 0);
-  sunMesh.scale.setScalar(4);
+  sunMesh.scale.setScalar(7);
+
   scene.add(sunMesh);
   scene.add(earthGroup);
   scene.add(marsGroup);
   scene.add(jupiterGroup);
+  scene.add(venusGroup);
+  scene.add(mercuryGroup);
+  // scene.add(saturnGroup);
 
-  const light = new THREE.PointLight('white', 2);
+  const light = new THREE.PointLight('white', 1);
   light.position.set(0, 0, 0);
   scene.add(light);
 
@@ -135,16 +196,21 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render({ time }) {
-      console.log(time);
-      earthMesh.rotation.y = time * 0.4;
-      earthGroup.rotation.y = time * 0.3;
+      earthMesh.rotation.y = time * 0.7;
+      earthGroup.rotation.y = time * 0.1;
       moonMesh.rotation.y = time * 0.2;
       moonGroup.rotation.y = time * 0.7;
       marsMesh.rotation.y = time * 0.4;
-      marsGroup.rotation.y = time * 0.15;
+      marsGroup.rotation.y = time * 0.07;
       jupiterMesh.rotation.y = time * 0.7;
-      jupiterGroup.rotation.y = time * 0.04;
-      sunMesh.rotation.y = time * 0.02;
+      jupiterGroup.rotation.y = time * 0.03;
+      saturnMesh.rotation.y = time * 0.3;
+      saturnGroup.rotation.y = time * 0.01;
+      venusMesh.rotation.y = -time * 0.02;
+      venusGroup.rotation.y = time * 0.15;
+      mercuryMesh.rotation.y = time * 0.3;
+      mercuryGroup.rotation.y = time * 0.4;
+      sunMesh.rotation.y = time * 0.1;
 
       controls.update();
       renderer.render(scene, camera);
